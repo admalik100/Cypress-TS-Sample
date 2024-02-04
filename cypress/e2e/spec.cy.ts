bd5cd7ac4270e6a118477511931274023e3e9b28
admalik100 <ahmad.amir.malick@gmail.com>
2024-02-05 02:08:36 +0500

    first test case with POM

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
+import Homepage from '../support/pages/Homepage';
+
+describe('Validate Product content on Homepage', () => {
+  const hp = new Homepage();
+
+  beforeEach('Preconditions', () => {
     cy.waitForProductLoading();
+  });
+  it('Validate all products have name and price displayed', () => {
+    hp.validateProductCardContent();
   });
 });
